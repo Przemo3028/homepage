4ec29cb4f05573b7e96a0561495f5a8d78210eed
Przemek Tomaka <przemo3028@gmail.com>
2022-07-12 15:58:05 +0200

    blocks added, names of classes changed

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let body = document.querySelector(".body");
 let button = document.querySelector(".button");
 let themeName = document.querySelector(".themeName");
 button.addEventListener("click", () => {
-    body.classList.toggle("js-bodySecondaryBackground");
-    themeName.innerText = body.classList.contains("js-bodySecondaryBackground") ? "Kremowe" : "Białe";
+    body.classList.toggle("body__SecondaryBackground");
+    themeName.innerText = body.classList.contains("body__SecondaryBackground") ? "Kremowe" : "Białe";
 });
 
